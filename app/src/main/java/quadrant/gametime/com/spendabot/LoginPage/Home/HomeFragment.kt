23984c6f6a16462016e1be3836bb9a0ef32780cd
akansh <akansh@promactinfo.com>
2018-05-08 11:37:37 +0530

    2nd commit

--- app/src/main/java/quadrant/gametime/com/spendabot/LoginPage/Home/HomeFragment.kt
+++ app/src/main/java/quadrant/gametime/com/spendabot/LoginPage/Home/HomeFragment.kt
@@ -2,11 +2,13 @@ package quadrant.gametime.com.spendabot.LoginPage.Home
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 import quadrant.gametime.com.spendabot.LoginPage.Utils.PrefManager
 
 import quadrant.gametime.com.spendabot.R
@@ -19,6 +21,8 @@ class HomeFragment : Fragment() {
     private lateinit var noBudget: TextView
     private lateinit var currDate: String
     private lateinit var progress: ProgressBar
+    private lateinit var auth: FirebaseAuth
+    private val TAG = HomeFragment::class.simpleName
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -27,6 +31,11 @@ class HomeFragment : Fragment() {
         monthName = rootView.findViewById(R.id.monthName)
         noBudget = rootView.findViewById(R.id.noBudget)
         progress = rootView.findViewById(R.id.expProgress)
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            Log.d(TAG, "username: ${auth.currentUser!!.displayName}")
+        }
 
         currDate = SimpleDateFormat("MMMM", Locale.getDefault()).format(Date())
         monthName.text = currDate
